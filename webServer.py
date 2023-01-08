218dc9082eddbfee4f1439c3af97c195494fecf2
MoranLevi <73950497+MoranLevi@users.noreply.github.com>
2023-01-08 12:16:22 +0200

    add cors to web server

--- webServer.py
+++ webServer.py
@@ -1,11 +1,20 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
+import sys
 
 class requestHandler(BaseHTTPRequestHandler):
+    def end_headers(self): # CORS
+        self.send_header('Access-Control-Allow-Origin', '*')
+        self.send_header('Access-Control-Allow-Methods', '*')
+        self.send_header('Access-Control-Allow-Headers', '*')
+        self.send_header('Cache-Control', 'no-store, no-cache, must-revalidate')
+        return super(requestHandler, self).end_headers()
+
     def do_GET(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()
-        self.wfile.write('Hello, world!'.encode())
+        if self.path.endswith('/optimal-targets-classification'):
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            self.wfile.write('Hello, world!'.encode())
 
 def main():
     PORT = 8000
