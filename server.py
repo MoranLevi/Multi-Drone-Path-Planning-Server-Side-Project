b6c185c89ee1924522efda24f87ed077403ac6ab
MoranLevi <73950497+MoranLevi@users.noreply.github.com>
2023-01-13 00:41:20 +0200

    add new format to server

---
+++ server.py
@@ -0,0 +1,38 @@
+from flask import Flask, Response,request, jsonify
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import json
+import algorithm
+from flask_cors import CORS, cross_origin
+
+numbersOfDrones = -1
+fileName = None
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/optimal-targets-classification', methods=['GET'])
+def get_optimal_numbers_of_drones():
+    # fileName="TSP100.txt"
+    fileName = request.args.get('fileName')
+    numbersOfDrones = -1
+    results = algorithm.start_algorithm(fileName, numbersOfDrones)
+    results = [1, 2, 3, 4, 5]
+    return jsonify(results)
+    #return results
+    # return Response(f'Accepted - {fileName}', status=202, mimetype='application/json')
+
+@app.route('/required-targets-classification', methods=['GET'])        
+def get_required_targets_classification():
+    fileName = request.args.get('fileName')
+    numberOfDrones = request.args.get('numberOfDrones')
+    print(numberOfDrones)
+    #results = algorithm.start_algorithm(fileName, numbersOfDrones)
+    #return results
+    results = algorithm.start_algorithm(fileName, numberOfDrones)
+    results = [1, 2, 3, 4, 5]
+    return jsonify(results)
+    # return Response('Accepted', status=202, mimetype='application/json')
+  
+
+if __name__ == '__main__':
+    app.run('localhost', '8000')
--- webServer.py
+++ webServer.py
@@ -1,5 +1,6 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
 import json
+import algorithm
 
 numbersOfDrones = -1
 fileName = None
@@ -17,16 +18,25 @@ class requestHandler(BaseHTTPRequestHandler):
       self.end_headers()
     
     def do_GET(self):
+        print("gggggggggggggggggggg")
         if self.path.endswith('/optimal-targets-classification'):
+            print("hhhhhhhhhhhhhhhhh")
             self.send_response(200)
             self.send_header('Content-type', 'text/html')
             self.end_headers()
-            self.wfile.write(numbersOfDrones.encode())
+            
+            results = algorithm.start_algorithm(fileName, -1)
+            print("after", results)
+            self.wfile.write(results.encode())
+
+            # self.wfile.write(numbersOfDrones.encode())
         if self.path.endswith('/required-targets-classification'):
             self.send_response(200)
             self.send_header('Content-type', 'text/html')
             self.end_headers()
             self.wfile.write('Hi!'.encode())
+            results = algorithm.start_algorithm(fileName, numbersOfDrones)
+            self.wfile.write(results.encode())
 
     def do_POST(self):
         global numbersOfDrones
@@ -38,9 +48,10 @@ class requestHandler(BaseHTTPRequestHandler):
             dataLength = int(self.headers["Content-Length"])
             data = self.rfile.read(dataLength)
             numbersOfDrones = json.loads(data).get("numberOfDrones")
-            print('Received number of drones: ' + numbersOfDrones)
+            print('Received number of drones: ' + str(numbersOfDrones))
             self.wfile.write('OK'.encode())
         if self.path.endswith('/fileNameData'):
+            print("jjjj")
             self.send_response(200)
             self.send_header("Content-Type", "application/json")
             self.end_headers()
@@ -56,6 +67,7 @@ def main():
     server = HTTPServer(('', PORT), requestHandler)
     print('Server running on port %s' % PORT)
     server.serve_forever()
+    
 
 if __name__ == '__main__':
     main()
